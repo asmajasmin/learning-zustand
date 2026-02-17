30a687f428a656b446db018d63228eb8867d4c4d
Asma Jasmin <asmajasmin217@gmail.com>
2026-02-17 12:43:15 +0530

    Added product component and hooks, improved search logic

---
+++ src/components/product.jsx
@@ -0,0 +1,47 @@
+
+import { useMemo } from "react";
+import useDebounce from "../hooks/useDebounce";
+
+const products = [
+  "Book",
+  "Pen",
+  "Pencil",
+  "Notebook",
+  "Calculator",
+  "Mouse",
+  "Keyboard",
+];
+
+const Products = ({ filter }) => {
+  const delayedData = useDebounce(filter, 500);
+
+  const results = useMemo(() => {
+    if (!delayedData) return products;
+    return products.filter((product) =>
+      product.toLowerCase().includes(delayedData.trim().toLowerCase()),
+    );
+  }, [delayedData]);
+  return (
+    <div className="w-full h-full flex flex-col items-center gap-y-10">
+      <h1 className="text-4xl font-bold tracking-tight">
+        {delayedData ? `Results for ${delayedData}` : "All Products"}
+      </h1>
+      {results.length === 0 ? (
+        <div>No result found for {delayedData}</div>
+      ) : (
+        <ul className="w-full max-w-xl">
+          {results.map((product, index) => (
+            <li
+              className="border rounded-sm px-4 py-2 mb-4 last:mb-0"
+              key={index}
+            >
+              {product}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Products;
